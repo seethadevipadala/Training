ebac9e3382be15caa6896d1335ba535d45946d65
seethadevipadala <seethadevipadala2000@gmail.com>
2022-07-04 12:22:20 +0530

    javascript fundamentals part1

---
+++ javascript/01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,27 @@
+let js= 'amazing'
+if (js==='amazing') alert('js is amazing')
+
+let a= 10
+console.log(a)
+
+a='sita'
+console.log(a)
+
+let aa= 10
+console.log(aa)
+
+aa=20
+console.log(aa)
+
+var aaa= 10
+console.log(aaa)
+
+aaa='sita'
+console.log(aaa)
+
+const x= '10'
+const xx=Number(x)
+
+const y= 55
+const xy = xx+y
+console.log(xy)
